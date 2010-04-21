3309198ff0ba3299b4a7aa5bb7dab5ab8fb74eaa
Michael Marino <mgmarino@mjcalc.npl.washington.edu>
2010-04-21 12:07:45 -0700

    Removing Sensitivity Calculation from this repo.

--- SensitivityCalculation/pyWIMP/WIMPPdfs/LinkDef.h
+++
@@ -1,9 +0,0 @@
-#pragma link C++ class MGMWimpDiffRatePdf+;
-#pragma link C++ class MGMWimpDiffRateBasicPdf+;
-#pragma link C++ class MGMVWimpFormFactor+;
-#pragma link C++ class MGMWimpDiffRateEscapeVelPdf+;
-#pragma link C++ class MGMWimpHelmFFSquared+;
-#pragma link C++ class MGMWimpTimeFunction+;
-#pragma link C++ class MGMPiecewiseFunction+;
-#pragma link C++ class MGMErfcFunction+;
-#pragma link C++ class MGMPolyPlusExponential+;
