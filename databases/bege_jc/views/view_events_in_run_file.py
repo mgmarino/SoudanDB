bcdd696da5419c325ebf84994f932c09ff89e26e
Michael Marino <mmarino@gmail.com>
2010-05-15 13:22:27 -0700

    Updating viewing events, tracking only the events after 4 Dec

--- databases/bege_jc/views/view_events_in_run_file.py
+++ databases/bege_jc/views/view_events_in_run_file.py
@@ -7,9 +7,15 @@ are found in the file.
 def get_view_class():
     return ViewDefinition("analysis", "all_events_in_runfile", \
     '''function(doc) {
-       var my_string = doc._id
-       if (my_string.search("LN2") != -1) return;
-       if (isNaN(parseInt(my_string))) return;
+       if (doc.ln_data_file) return;
+       var int_of_doc_id = parseInt(doc._id);
+       if (isNaN(int_of_doc_id)) return;
+       // We don't accept any runs before 4 December.  
+       // The runs before this were with a different DAQ system.
+       // i.e. no digitized pulses.
+       if (int_of_doc_id < 20091204180000) {
+         return;
+       }
        if (! doc.number_of_entries_in_tier1_root_tree) return;
        emit(doc.number_of_entries_in_tier1_root_tree, 1); 
     }
