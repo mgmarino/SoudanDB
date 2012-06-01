cbeb1b776d1d6934b7b6159f63047ac1a1284c5e
Michael Marino <mgmarino@mjcalc.npl.washington.edu>
2012-06-01 06:45:41 -0700

    Properly auto-adding views to the module when imported

--- databases/bege_jc/views/__init__.py
+++ databases/bege_jc/views/__init__.py
@@ -1,11 +1,13 @@
 import SoudanDB.views
+import pkgutil
 dirname = SoudanDB.views.__path__[0]
 __path__.insert(0, dirname)
-#import pkgutil
-#module_dict = globals()
-#current_db_name = SoudanDB.views.__name__
-#for loader,name,ispkg in pkgutil.iter_modules([SoudanDB.views.__path__[0]]):
-#    if ispkg: continue
-#    load = pkgutil.find_loader('%s.%s' % (current_db_name, name))
-#    mod = load.load_module("%s.%s" % (current_db_name,name))
-#    module_dict[mod.__name__] = mod
+module_dict = globals()
+current_db_name = SoudanDB.views.__name__
+# Now export all submodules
+__all__ = []
+for loader,name,ispkg in pkgutil.walk_packages( __path__ ):
+    if ispkg: continue
+    __all__.append(name)
+    module = loader.find_module(name).load_module(name)
+    module_dict[name] = module
